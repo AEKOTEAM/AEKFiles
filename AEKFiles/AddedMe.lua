a4616d1b153af1986bf737159076c18722013c10
AEKOTEAM <60983866+AEKOTEAM@users.noreply.github.com>
2021-02-24 22:15:06 +0300

    Add files via upload

--- AEKFiles/AddedMe.lua
+++ AEKFiles/AddedMe.lua
@@ -2,10 +2,10 @@ local function AddedMe(msg)
 local text = msg.content_.text_
 if ChatType == 'sp' or ChatType == 'gp'  then
 if text and text:match("منو ضافني") then
-if not DevAek:get(AEK..'aek:Added:Me'..msg.chat_id_) then
+if not DevAek:get(AEK..'Aek:Added:Me'..msg.chat_id_) then
 tdcli_function ({ID = "GetChatMember",chat_id_ = msg.chat_id_,user_id_ = msg.sender_user_id_},function(arg,da) 
 if da and da.status_.ID == "ChatMemberStatusCreator" then
-Dev_aek(msg.chat_id_, msg.id_, 1, '♚∫ انت منشئ المجموعه', 1, 'md') 
+Dev_Aek(msg.chat_id_, msg.id_, 1, '♚∫ انت منشئ المجموعه', 1, 'md') 
 return false
 end
 local Added_Me = DevAek:get(AEK.."Who:Added:Me"..msg.chat_id_..':'..msg.sender_user_id_)
@@ -16,21 +16,21 @@ Text = '♚∫ *الشخص الذي قام باضافتك هو* ↫ '..Name
 SendText(msg.chat_id_,Text,msg.id_/2097152/0.5,'md')
 end,nil)
 else
-Dev_aek(msg.chat_id_, msg.id_, 1, '♚∫ انت دخلت عبر الرابط', 1, 'md') 
+Dev_Aek(msg.chat_id_, msg.id_, 1, '♚∫ انت دخلت عبر الرابط', 1, 'md') 
 end
 end,nil)
 else
-Dev_aek(msg.chat_id_, msg.id_, 1, '♚∫ امر منو ضافني تم تعطيله من قبل المدراء', 1, 'md') 
+Dev_Aek(msg.chat_id_, msg.id_, 1, '♚∫ امر منو ضافني تم تعطيله من قبل المدراء', 1, 'md') 
 end
 end
 
 if text == 'تفعيل ضافني' and Manager(msg) then 
-DevAek:del(AEK..'aek:Added:Me'..msg.chat_id_) 
-Dev_aek(msg.chat_id_, msg.id_, 1, '♚∫ تم تفعيل امر منو ضافني', 1, 'md') 
+DevAek:del(AEK..'Aek:Added:Me'..msg.chat_id_) 
+Dev_Aek(msg.chat_id_, msg.id_, 1, '♚∫ تم تفعيل امر منو ضافني', 1, 'md') 
 end
 if text == 'تعطيل ضافني' and Manager(msg) then 
-DevAek:set(AEK..'aek:Added:Me'..msg.chat_id_,true) 
-Dev_aek(msg.chat_id_, msg.id_, 1, '♚∫ تم تعطيل امر منو ضافني', 1, 'md') 
+DevAek:set(AEK..'Aek:Added:Me'..msg.chat_id_,true) 
+Dev_Aek(msg.chat_id_, msg.id_, 1, '♚∫ تم تعطيل امر منو ضافني', 1, 'md') 
 end
 end
 
